5b493b52487695483ce731a6eaf5ed4afbd33fc7
atlas7 <johnnychan0302@gmail.com>
2016-05-30 16:04:28 +0100

    add pure mixin

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -88,7 +88,7 @@ describe('Voting', () => {
       container
     );
     firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
-    expect(firstButton.textContent).to.equal('Sunshine');
+    expect(firstButton.textContent).to.equal('Trainspotting');
   });
 
   it('does update DOM when prop changes', () => {
