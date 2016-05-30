0d95b1320b70d11362184ef7a84a4257067bfbad
atlas7 <johnnychan0302@gmail.com>
2016-05-30 15:12:55 +0100

    add unit tests, vote, and winner

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,10 @@ import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+// <Voting pair={pair} hasVoted="Trainspotting" winner="Trainspotting"/>,
+// <Voting pair={pair} winner="Trainspotting" />,
+// <Voting pair={pair} hasVoted="Trainspotting" />,
 ReactDOM.render(
-<Voting pair={pair} />,
+<Voting pair={pair} hasVoted="Trainspotting"/>,
   document.getElementById('app')
 );
